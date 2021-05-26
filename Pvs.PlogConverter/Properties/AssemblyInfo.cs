9a7b95a052e967a8ea24c02934d4e35cc721da19
Svyatoslav Razmyslov <razmyslov@viva64.com>
2021-05-26 11:39:04 +0300

    7.13

--- Pvs.PlogConverter/Properties/AssemblyInfo.cs
+++ Pvs.PlogConverter/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("7.12.0.0")]
-[assembly: AssemblyFileVersion("7.12.0.0")]
+[assembly: AssemblyVersion("7.13.0.0")]
+[assembly: AssemblyFileVersion("7.13.0.0")]
